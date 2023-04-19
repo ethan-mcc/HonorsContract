50404e6b8ff3381f084901b1703f0c2c8a1bf496
ethan-mcc <ethanmccaskey2@gmail.com>
2023-04-19 13:41:23 -0700

    Latency improvements

--- webapp/src/routes/+layout.server.ts
+++ webapp/src/routes/+layout.server.ts
@@ -1,7 +1,9 @@
 import type { LayoutServerData } from './$types';
+import {connectToDB} from "../lib/utils/db";
 
 export const load = (async ({ locals: { locale, LL } }) => {
 	//console.info(LL.log({ fileName: '+layout.server.ts' }));
 	// pass locale information from "server-context" to "shared server + client context"
+	await connectToDB()
 	return { locale };
 }) satisfies LayoutServerData;
--- webapp/src/routes/api/v/+server.ts
+++ webapp/src/routes/api/v/+server.ts
@@ -5,7 +5,7 @@ import sql from "mssql";
 
 // Custom API EndPoint
 export const GET: RequestHandler = async (event) => {
-    await connectToDB()
+
     const result = await sql.query`select 
 items_name, items_location, items_country, items_latitude, items_longitude, items_elevation, items_morphology, items_vei 
 from dbo.volcanoes`
